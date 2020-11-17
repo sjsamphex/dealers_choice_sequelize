212c28289a22bf946085f1ff5f1371f61a86c213
sjsamphex <sjsamphex@gmail.com>
2020-11-17 14:16:34 -0800

    prepare for heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const fourOhFour = require('./views/404');
 
 const { conn, syncAndSeed } = require('./db/index');
